a952dade3e84ec747b8f623ce872333474869f56
minhIMT <61503934+minhIMT@users.noreply.github.com>
2021-11-21 21:43:45 +0100

    Update workflow in main.py and some functions in utils.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from pandas_profiling import ProfileReport
+from sklearn import svm
 import numpy as np
 import matplotlib.pyplot as plt
 # Self-define utils
@@ -8,19 +8,31 @@ from utils import *
 
 
 if __name__ == "__main__":
-	# infile = "kidney_disease.csv"
-    # feature_list = ['age', 'bp', 'sg', 'al', 'su', 'rbc', 'pc', 'pcc', 'ba', 'bgr', 'bu', 'sc', 'sod', 'pot', 'hemo', 'pcv', 'wc', 'rc', 'htn', 'dm', 'cad', 'appet', 'pe', 'ane']
+    infile = "kidney_disease.csv"
+    feature_list = ['age', 'bp', 'sg', 'al', 'su', 'rbc', 'pc', 'pcc', 'ba', 'bgr', 'bu', 'sc', 'sod', 'pot', 'hemo', 
+                    'pcv', 'wc', 'rc', 'htn', 'dm', 'cad', 'appet', 'pe', 'ane']
 
-	infile = "data_banknote_authentication.txt"
-	feature_list = ["variance","skewness","curtosis","entropy"]
-
-	train_dataset, valid_dataset, test_dataset, train_loader, valid_loader, test_loader = split_data(infile, feature_list, batch_size=32)
-	
+    # infile = "data_banknote_authentication.txt"
+    # feature_list = ["variance","skewness","curtosis","entropy"]
+    
+    train_dataset, valid_dataset, test_dataset, train_loader, valid_loader, test_loader = split_data(infile, feature_list)
 	# Read dataloader
-	# return List
-	for data, label in train_dataset:
-		pass
-
-	# return Tensor
-	for data, label in train_loader:
-		pass
+    X_train, Y_train, X_test, Y_test = [], [], [], []
+    for data, label in train_dataset:
+        X_train.append(parsing_data(data))
+        Y_train.append(label)
+    for data, label in test_dataset:
+        X_test.append(parsing_data(data))
+        Y_test.append(label)
+    # Convert to numpy array
+    X_train = np.array(X_train)
+    Y_train = np.array(Y_train)
+    X_test = np.array(X_test)
+    Y_test = np.array(Y_test)
+    # Train SVM model with train data
+    clf = svm.SVC(kernel = "rbf", C = 3, gamma = 0.3)
+    clf.fit(X_train, Y_train)
+    # Test trained svm model and print accuracy
+    error = clf.predict(X_test) - Y_test
+    accuracy = len(error[error == 0])/len(error)
+    print("Accuracy of the SVM model: ",accuracy)
--- utils.py
+++ utils.py
@@ -7,9 +7,31 @@ Created on Fri Nov 19 22:31:39 2021
 
 import os
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+
+def category2numerical(df):
+    '''
+    Convert category to numeric value for classification
+    '''
+    cat_columns = df.select_dtypes(['category']).columns
+    df[cat_columns] = df[cat_columns].apply(lambda x: x.cat.codes)
+    return df 
+
+def normalize_data(df):
+    '''
+    Normalize the data. This is often applied with kidney dataset.
+    '''
+    # Choose features with int64 or float64
+    num_features = []
+    for features in df.select_dtypes(include=['int64','float64']):
+        num_features.append(features)
+    # Scaling numerical features
+    minmax = MinMaxScaler()
+    df[num_features] = minmax.fit_transform(df[num_features].values)
+    return df
 
 def clean_data(infile):
 	"""
@@ -30,10 +52,10 @@ def clean_data(infile):
 		df = df.drop(columns="id")
 
 		# Handle "	?" values
-		nodataval = "?"
+		nodataval = "\t?"
 		df = df.replace(nodataval, np.nan)
-    # df = df.str.replace('\t',"")
-    # df = df.str.replace(' ',"")
+        # df = df.str.replace('\t',"")
+        # df = df.str.replace(' ',"")
 
 		# Convert false string columns
 		other_numeric_columns = ["pcv", "wc", "rc"]
@@ -59,18 +81,21 @@ def clean_data(infile):
 
 		# Further cleaning
 		df['classification'] = df['classification'].replace(to_replace={"ckd\t":"ckd","\tckd":"ckd"})
+		df['classification'] = df['classification'].astype("category")
 		df['cad'] = df['cad'].replace("\tno","no")
+		df['cad'] = df['cad'].astype("category")
 		df['dm'] = df['dm'].replace(to_replace={"\tno":"no","\tyes":"yes"," yes":"yes"})
-    
+		df['dm'] = df['dm'].astype("category")
+
+        # Preprocess
+		df = normalize_data(df)
+		df = category2numerical(df)
+
 		return df
 
 	else:
 		print("This is not the dataset we were to deal with. Please check its name to make sure it matches one of these two:\n", "data_banknote_authentication.txt\n", "kidney_disease.csv\n")
 
-def category2numerical(df):
-  cat_columns = df.select_dtypes(['category']).columns
-  df[cat_columns] = df[cat_columns].apply(lambda x: x.cat.codes)
-  return df 
 
 # Define dataset
 class binary_classification_dataset(torch.utils.data.Dataset):
@@ -119,43 +144,53 @@ def split_data(infile, feature_list, batch_size=32):
 	train_dataset, valid_dataset = torch.utils.data.random_split(dataset=left_dataset,lengths=[len(left_dataset)-valiset_length, valiset_length])
 
 	# Load dataset as dataloader 
-	train_loader = torch.utils.data.DataLoader(train_dataset, shuffle=True)
-	valid_loader = torch.utils.data.DataLoader(valid_dataset, shuffle=False)
-	test_loader = torch.utils.data.DataLoader(test_dataset, shuffle=False)
+	train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = batch_size, shuffle=True)
+	valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size = batch_size, shuffle=False)
+	test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = batch_size, shuffle=False)
 
 	return train_dataset, valid_dataset, test_dataset, train_loader, valid_loader, test_loader
 
-def split_data_numpy(df_file, feature_list):
-  """
-  This function outputs numpy format
-  Split between training set and test set
-  Split the training set for cross-validation
-  Inputs: 
-    - df: 	
-      type of pandas.core.frame.DataFrame
-      cleaned data of Banknote Authentication Dataset or Chronic Kidney Disease
+def parsing_data(data):
+    '''
+    Convert data from dict to array
+    '''
+    new_data = []
+    for key in data:
+      new_data.append(data[key])
+    return new_data
 
-  Outputs:
-    - train data:
-    - valid data:
-    - test data : 
-  """
-  dataset = df_file.to_numpy()
-  # split ratio for training, testing and validation dataset
-  testset_ratio = 0.2
-  valiset_ratio = 0.1
-  testset_length = int((dataset.shape[0])*testset_ratio)
-  valiset_length = int((dataset.shape[0])*valiset_ratio)
-  trainset_length = dataset.shape[0] - testset_length - valiset_length
 
-  # Random shuffle dataset
-  np.random.shuffle(dataset)
-  # Split dataset
-  X_train = dataset[:trainset_length, :-1]
-  X_test = dataset[trainset_length:trainset_length + testset_length, :-1]
-  Y_train = dataset[:trainset_length, -1]
-  Y_test = dataset[trainset_length:trainset_length + testset_length, -1]
-  return X_train, Y_train, X_test, Y_test
+# def split_data_numpy(df_file, feature_list):
+#   """
+#   This function outputs numpy format
+#   Split between training set and test set
+#   Split the training set for cross-validation
+#   Inputs: 
+#     - df: 	
+#       type of pandas.core.frame.DataFrame
+#       cleaned data of Banknote Authentication Dataset or Chronic Kidney Disease
+
+#   Outputs:
+#     - train data:
+#     - valid data:
+#     - test data : 
+#   """
+#   dataset = df_file.to_numpy()
+#   # split ratio for training, testing and validation dataset
+#   testset_ratio = 0.2
+#   valiset_ratio = 0.1
+#   testset_length = int((dataset.shape[0])*testset_ratio)
+#   valiset_length = int((dataset.shape[0])*valiset_ratio)
+#   trainset_length = dataset.shape[0] - testset_length - valiset_length
+
+#   # Random shuffle dataset
+#   np.random.shuffle(dataset)
+#   # Split dataset
+#   X_train = dataset[:trainset_length, :-1]
+#   X_test = dataset[trainset_length:trainset_length + testset_length, :-1]
+#   Y_train = dataset[:trainset_length, -1]
+#   Y_test = dataset[trainset_length:trainset_length + testset_length, -1]
+#   return X_train, Y_train, X_test, Y_test
 
 
 
